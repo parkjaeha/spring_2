ac0c955d071e7ec0c882c80d40b2dd0effed2fc7
user1-PC\user1 <ssin226@naver.com>
2017-11-28 13:50:32 +0900

    Initial commit

---
+++ src/main/java/com/iu/Board/BoardDAO.java
@@ -0,0 +1,5 @@
+package com.iu.Board;
+
+public class BoardDAO{
+
+}
---
+++ src/main/java/com/iu/Board/BoardDTO.java
@@ -0,0 +1,14 @@
+package com.iu.Board;
+
+import java.util.Date;
+
+public class BoardDTO {
+
+	private int num;
+	private String writer;
+	private String title;
+	private String contents;
+	private Date reg_date;
+	private int hit;
+	
+}
